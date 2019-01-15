2f797b28bb2ce93ff9d06e73b01089c1cbb1cd3a
Rad Cirskis <nad2000@gmail.com>
2019-01-15 21:42:29 +0900

    file reading and parsing and stuff

---
+++ parsing_text_in_file.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <vector>
+#include <utility>
+using namespace std;
+
+static const string fileName = "stats.txt";
+
+vector<pair<string,int>> readLines(ifstream& ifs) {
+	vector<pair<string,int>> lines{};
+	string country;
+	int population;
+	while(ifs) { // == !ifs.eof()
+		getline(ifs, country, ':');
+		ifs >> population;
+		ifs >> ws; // reads up 'white spaces' or use ifs.get(); // to digest '\n'
+		if (country != "") lines.push_back(make_pair(country, population));
+	}
+	return lines;
+}
+
+int main() {
+	ifstream inFile; // or fstream and then open with 'ios:in'
+
+	inFile.open(fileName);
+	
+	if(inFile.is_open()) {
+		auto lines = readLines(inFile);
+		int lineNo = 1;
+		for (const auto& l: lines) {
+			cout << lineNo++ << ":" << l.first << " -- " << l.second << endl;
+		}
+		inFile.close();
+	} else {
+		cout 
+			<< "Cannot open file '" 
+			<< fileName << "'" 
+			<< endl;
+		return -1;
+	}
+}
---
+++ read_text_file.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <vector>
+using namespace std;
+
+vector<string> readLines(ifstream& ifs) {
+	vector<string> lines{};
+	string line;
+	while(ifs) { // == !ifs.eof()
+		getline(ifs, line);
+		if (line != "") lines.push_back(line);
+	}
+	return lines;
+}
+
+int main() {
+	const char* fileName = "test.txt";
+	ifstream inFile; // or fstream and then open with 'ios:in'
+
+	inFile.open(fileName);
+	
+	if(inFile.is_open()) {
+		auto lines = readLines(inFile);
+		int lineNo = 1;
+		for (const auto& l: lines) {
+			cout << lineNo++ << ":" << l << endl;
+		}
+		inFile.close();
+	} else {
+		cout 
+			<< "Cannot open file '" 
+			<< fileName << "'" 
+			<< endl;
+	}
+}
