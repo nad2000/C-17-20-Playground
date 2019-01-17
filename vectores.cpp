e5fd92fe4a45bfc64ed3c4f7d59f3eeddeb3fb82
Rad Cirskis <nad2000@gmail.com>
2019-01-17 20:09:45 +0900

    fun with iterators

--- vectores.cpp
+++ vectores.cpp
@@ -10,11 +10,15 @@ int main() {
   strings[3] = "dog";
   cout << strings[3] << endl;
   cout << "SIZE: " << strings.size() << endl;
+  strings.push_back("one");
+  strings.push_back("two");
+  strings.push_back("three");
 
   for (unsigned int i = 0; i < strings.size(); i++) {
     cout << i << ": " << strings[i] << endl;
   }
 
+  // with an iterator
   for (auto it = strings.begin(); it != strings.end(); ++it)
     // cout << (it - strings.begin()) << ": " << *it << endl;
     cout << distance(strings.begin(), it) << ": " << *it << endl;
