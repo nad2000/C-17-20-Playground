0e6453a2ab2a924a5a39b43662776b1855618935
Rad Cirskis <nad2000@gmail.com>
2019-01-22 19:34:20 +0900

    stacks and queues...

--- stacks_and_queues.cpp
+++ stacks_and_queues.cpp
@@ -13,6 +13,8 @@ public:
   Test(int id, string name) : id(id), name(name) {}
   Test(const Test &t) : id(t.id), name(t.name) {}
   Test() {}
+  ~Test() { cout << "Object destroyed" << endl; }
+
   friend ostream &operator<<(ostream &out, const Test &t) {
     return out << "Test[id: " << t.id << ", name: '" << t.name << "']";
   }
@@ -20,5 +22,32 @@ public:
   bool operator<(const Test &other) const { return name < other.name; }
 };
 
-int main() { return 0; }
+int main() {
+
+  stack<Test> testStack;
+
+  testStack.push(Test(11, "Mike"));
+  testStack.push(Test(12, "John"));
+  testStack.push(Test(42, "Sue"));
+  cout << "Top: " << testStack.top() << endl;
+  // NB! BEWARE!!!
+  auto &test1 = testStack.top();
+  cout << "Still here (reference to destroyed): " << test1 << endl;
+  testStack.pop();
+  cout << "Top: " << testStack.top() << endl;
+
+  // FIFO
+  queue<Test> testQueue;
+  testQueue.push(Test(11, "Mike"));
+  testQueue.push(Test(12, "John"));
+  testQueue.push(Test(42, "Sue"));
+
+  while (testQueue.size() > 0) {
+    Test &test = testQueue.front();
+    cout << "Test: " << test << endl;
+    testQueue.pop();
+  }
+
+  return 0;
+}
 
