40c793988d1dbd6c8cdfced6c287ecce2fe0fccc
Rad Cirskis <nad2000@gmail.com>
2019-01-30 20:46:56 +0900

    template functios and type inference for the argument.

---
+++ template_function_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// template<class T>
+template <typename T> // the same as 'class'
+void print(const T &n) {
+  cout << "Template version: " << n << endl;
+}
+
+void print(const int &n) { cout << "Non-template version: " << n << endl; }
+
+template <class T> void show() { cout << "T(): " << T() << endl; }
+
+int main() {
+  print<int>(42);
+  print<string>("Hello World!");
+  // no need to specify type, C++ can infer them
+  print(
+      42); // though since here closer match will be the non-template function.
+  // but themplated version can be denoted wiht <>
+  print<>(42);
+  print("Hello World!");
+  // cannot infer with
+  // show();
+  // show<>();
+  // BUT:
+  show<double>(); // is fine.
+  show<int>();    // is fine.
+
+  return 0;
+}
+
