e4ea08f9c7270c4d0bdf431ab55d3ac4058c832d
Rad Cirskis <nad2000@gmail.com>
2019-02-06 15:40:52 +0900

    functors and stuff

--- abstract_class.cpp
+++ abstract_class.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Animal {
 public:
   virtual void speak() = 0;
+  virtual void run() = 0;
 };
 
 class Dog : public Animal {
@@ -14,9 +15,23 @@ public:
   }
 };
 
+class Labrador : public Dog {
+public:
+  Labrador() { cout << "New Labrador" << endl; }
+  void run() { // virtual can be omitted. It will be "virtual" anyway.
+    cout << "Labrador is running!" << endl;
+  }
+};
+
 int main() {
-  Dog d;
-  d.speak();
+  Labrador l;
+  l.speak();
+  l.run();
+
+  Animal *animals[5];
+  animals[0] = &l;
+  animals[0]->speak();
+  animals[0]->run();
 
   return 0;
 }
