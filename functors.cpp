42d32af3ca5e22be6e4d0a7eaf19939ddb1fd9a9
Rad Cirskis <nad2000@gmail.com>
2019-02-12 22:02:57 +1300

    nice functor...

--- functors.cpp
+++ functors.cpp
@@ -3,16 +3,23 @@
 using namespace std;
 
 struct Test {
-  virtual bool operator()(const string &s) = 0;
+  virtual bool operator()(const string &s) const = 0;
+  virtual ~Test() {}
 };
 
+// Example of functor:
 struct Pred : Test {
-  bool operator()(const string &s) { return s == "lion"; }
+  bool operator()(const string &s) const { return s == "lion"; }
 };
+
+void check(const string &text, const Test &t) {
+  cout << (t(text) ? "match" : "no match") << endl;
+}
 
 int main() {
   Pred p;
-  cout << p("life") << endl;
+  check("lion", p);
+  check("life", p);
 
   return 0;
 }
